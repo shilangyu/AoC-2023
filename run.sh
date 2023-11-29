ab148b500ab7b62cd6456742d5fb89104f8c2fce
shilangyu <xmarcinmarcin@gmail.com>
2023-11-30 00:27:42 +0100

    Fix run script

--- run.sh
+++ run.sh
@@ -4,17 +4,21 @@ set -e
 ALL_DAYS=( ["1"]="lean" ["2"]="koka" )
 
 args=("$@")
-DAYS=("${args[@]:-${ALL_DAYS[@]}}")
+DAYS=("${args[@]:-${!ALL_DAYS[@]}}")
 
 for day in "${DAYS[@]}"; do
 	printf -v padded "%02d" $day
 	
 	case "${ALL_DAYS[$day]}" in
   "lean")
+    echo ""
+    echo "Day $day:"
     lean src/day$padded.lean --run
     ;;
 
   "koka")
+    echo ""
+    printf "Day $day:"
     koka --execute --verbose=0 src/day$padded.kk
     ;;
 
