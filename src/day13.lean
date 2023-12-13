9f8c013fc5b0e6964664a6de9ae56b1ad4efa178
shilangyu <xmarcinmarcin@gmail.com>
2023-12-13 10:52:28 +0100

    Add day 13 part 2

--- src/day13.lean
+++ src/day13.lean
@@ -7,20 +7,31 @@ partial def transpose [Inhabited α] : List (List α) → List (List α)
 def parseInput (s : String) : List (List Bool) :=
   List.map (fun e => List.map (· == '#') e.toList) (s.splitOn "\n")
 
-def symmetryCount (l : List (List Bool)) := count [] l
+def differenceCountRow (left right : List Bool) := match left, right with
+  | h1 :: t1, h2 :: t2 => (h1 != h2).toUInt64.toNat + differenceCountRow t1 t2
+  | _, _ => 0
+
+def differenceCount (left right : List (List Bool)) := match left, right with
+  | h1 :: t1, h2 :: t2 => differenceCountRow h1 h2 + (differenceCount t1 t2)
+  | _, _ => 0
+
+def symmetryCount (l : List (List Bool)) (smudgeCount : Nat) := count [] l smudgeCount
 where
-  count left right :=
+  count left right smudgeCount :=
     let s := Nat.min left.length right.length
-    (if left.reverse.take s == right.take s then left.length else 0) + match right with
-      | h1 :: h2 :: t => count (left ++ [h1]) (h2 :: t)
+    (if  differenceCount (left.reverse.take s) (right.take s) == smudgeCount then left.length else 0) + match right with
+      | h1 :: h2 :: t => count (left ++ [h1]) (h2 :: t) smudgeCount
       | _ => 0
 
+def summary (l : List (List (List Bool))) (smudgeCount : Nat) :=
+  (List.foldl (fun acc curr => acc + (symmetryCount (transpose curr) smudgeCount)) 0 l) +
+  100 * (List.foldl (fun acc curr => acc + (symmetryCount curr smudgeCount)) 0 l)
+
 def part1 (l : List (List (List Bool))) :=
-  (List.foldl (fun acc curr => acc + (symmetryCount (transpose curr))) 0 l) +
-  100 * (List.foldl (fun acc curr => acc + (symmetryCount curr)) 0 l)
+  summary l 0
 
 def part2 (l : List (List (List Bool))) :=
-  0
+  summary l 1
 
 def main : IO Unit := do
   let file ← IO.FS.readFile "input/day13.txt"
