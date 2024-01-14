aade182bc68402bfbae4bde948df79cb356bf6dc
shilangyu <xmarcinmarcin@gmail.com>
2024-01-14 18:47:24 +0100

    Add day 14 part 2

--- src/day14.lean
+++ src/day14.lean
@@ -26,22 +26,19 @@ where
     | '#' => Space.fixed
     | _ => Space.empty
 
-partial def part1 (l : List (List Space)) :=
-  List.map load (transpose l) |> List.foldl (· + ·) 0
+def load (l : List (List Space)) :=
+  List.map aux (transpose l) |> List.foldl (· + ·) 0
 where
-  load row :=
-    let untilStuck := List.takeWhile (· != Space.fixed) row
-    let count := List.filter (· == Space.rolling) untilStuck |> List.length
-    -- arithmetic sequence of length `count` starting from row.length and decreasing by one:
-    -- ∑_k=0^count (row.length - k)
-    let thisLoad := List.iota row.length |> List.take count |> List.foldl (· + ·) 0
-
-    thisLoad + (if row.isEmpty then 0 else load (List.drop (untilStuck.length + 1) row))
+  aux row :=
+    let rec aux2
+      | [], _ => 0
+      | h :: t, i => aux2 t (i-1) + if h == Space.rolling then i else 0
+    aux2 row (List.length row)
 
 partial def rollWest (l : List (List Space)) : List (List Space) :=
   List.map rollRow l
 where
-  rollRow := fun
+  rollRow
     | [] => []
     | Space.fixed :: t => Space.fixed :: rollRow t
     | row =>
@@ -51,11 +48,27 @@ where
       ++ (List.replicate (untilStuck.length - count) Space.empty)
       ++ rollRow (List.drop untilStuck.length row)
 
+def part1 := load ∘ transpose ∘ rollWest ∘ transpose
+
 def cycle : (l : List (List Space)) → List (List Space) :=
   rotateRight ∘ rotateRight ∘ rollWest ∘ rotateRight ∘ rollWest ∘ rotateRight ∘ rollWest ∘ rotateRight ∘ rollWest ∘ rotateLeft
 
-def part2 (l : List (List Space)) :=
-  0
+abbrev Memory := List (List (List Space))
+
+def indexOf := aux 0
+where
+  aux i l (m : Memory) := match m with
+    | [] => none
+    | h :: t => if h == l then some i else aux (i+1) l t
+
+partial def part2 (l : List (List Space)) :=
+  acc [l] l
+where
+  acc mem curr :=
+    let new := cycle curr
+    match indexOf new mem with
+    | some i => load (List.get! mem (i + (1000000000 - i) % (List.length mem - i)))
+    | none => acc (mem ++ [new]) new
 
 def main : IO Unit := do
   let file ← IO.FS.readFile "input/day14.txt"
